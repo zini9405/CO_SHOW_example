211c5376936d023a0612b6fa5a57997cede7e869
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2024-12-05 14:29:23 +0900

    test.py 업데이트

--- test.py
+++ test.py
@@ -1,28 +1,49 @@
-csv 파일에 HST_REG_DTTM 열으 시간을 나타내는 값이 있어.
-예) 20240101070004090000 -> 2024-01-01 07:00:04 
-예)처럼 변경주는 코드 구현
-
-
 import pandas as pd
 
-# 예시 데이터프레임 생성
-data = {'HST_REG_DTTM': ['20240101070004090000', '20240202080005090000']}
+# 예시 데이터
+data = {
+    'EQP_ID': ['E1', 'E1', 'E1', 'E2'],
+    'MODULE_NAME': ['M1', 'M1', 'M2', 'M2'],
+    'WAF_ID': ['W1', 'W1', 'W1', 'W2'],
+    'RECIPE_ID': ['R1', 'R1', 'R1', 'R2'],
+    'STEP_ID': [0, 1, 3, 0],
+    'STEP_NAME': ['Step0', 'Step1', 'Step3', 'Step0'],
+    'HST_REG_DTTM': ['20240101070004090000', '20240101070004090000', '20240101070004090000', '20240202080005090000'],
+    'MEASURE_1': [10, 20, 30, 40],
+    'MEASURE_2': [15, 25, 35, 45],
+}
 df = pd.DataFrame(data)
 
-# 변환 함수 정의
-def format_hst_reg_dttm(value):
-    return f"{value[:4]}-{value[4:6]}-{value[6:8]} {value[8:10]}:{value[10:12]}:{value[12:14]}"
+# STEP_ID 0부터 13까지 보장하도록 채우기
+all_steps = list(range(14))  # 0부터 13까지의 값
+grouped = []
 
-# HST_REG_DTTM 열 변환
-df['HST_REG_DTTM'] = df['HST_REG_DTTM'].apply(format_hst_reg_dttm)
+for waf_id, group in df.groupby('WAF_ID'):
+    group = group.set_index('STEP_ID')  # STEP_ID를 인덱스로 설정
+    missing_steps = set(all_steps) - set(group.index)  # 누락된 STEP_ID 찾기
+    for step in missing_steps:
+        # 누락된 STEP_ID에 대해 값 채우기
+        new_row = {
+            'EQP_ID': group['EQP_ID'].iloc[0],
+            'MODULE_NAME': group['MODULE_NAME'].iloc[0],
+            'WAF_ID': waf_id,
+            'RECIPE_ID': group['RECIPE_ID'].iloc[0],
+            'STEP_ID': step,
+            'STEP_NAME': f'Step{step}',
+            'HST_REG_DTTM': group['HST_REG_DTTM'].iloc[0],
+        }
+        # 나머지 값은 -1로 채우기
+        for col in df.columns:
+            if col not in new_row:
+                new_row[col] = -1
+        group = group.append(pd.DataFrame([new_row]).set_index('STEP_ID'))
+    grouped.append(group.sort_index())  # STEP_ID 순서대로 정렬
+
+# 다시 병합
+df_filled = pd.concat(grouped).reset_index()
+
+# WAF_ID 병합
+df_filled['WAF_ID'] = df_filled['WAF_ID'].astype(str).groupby(df_filled['WAF_ID']).transform('first')
 
 # 결과 출력
-print(df)
-
-
-나는 WAF_ID열을 묶어서 STEP_ID열을 0부터 13까지 값이 있을거야. 단, 0부터 13까지 중 없는 숫자가 있으면
-EQP_ID	MODULE_NAME	WAF_ID RECIPE_ID STEP_ID STEP_NAME HST_REG_DTTM 빼고 나머지 열의 값은 -1로 채워줘.
-그리고 묶은 WAF_ID을 다시 하나의 WAF_ID로 만들어줘.
-
-그리고 -1로 채운 값은 모델에 입력으로 사용할때 학습 안되면 좋겠는데 어뜨케하면 좋을까?
-
+print(df_filled)
