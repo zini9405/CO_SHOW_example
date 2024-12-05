7267b4a7e8f6c449ab24942a1a3424de41e75b13
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2024-12-05 14:27:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -18,3 +18,11 @@ df['HST_REG_DTTM'] = df['HST_REG_DTTM'].apply(format_hst_reg_dttm)
 
 # 결과 출력
 print(df)
+
+
+나는 WAF_ID열을 묶어서 STEP_ID열을 0부터 13까지 값이 있을거야. 단, 0부터 13까지 중 없는 숫자가 있으면
+EQP_ID	MODULE_NAME	WAF_ID RECIPE_ID STEP_ID STEP_NAME HST_REG_DTTM 빼고 나머지 열의 값은 -1로 채워줘.
+그리고 묶은 WAF_ID을 다시 하나의 WAF_ID로 만들어줘.
+
+그리고 -1로 채운 값은 모델에 입력으로 사용할때 학습 안되면 좋겠는데 어뜨케하면 좋을까?
+
