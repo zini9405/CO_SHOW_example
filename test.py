c1b0079e6e2529b4e8e6f9d2d2581357d9957f26
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2024-12-09 16:10:24 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,29 @@
-import pandas as pd
-from torch.utils.data import DataLoader, Dataset
-import torch
+Cell In[4], line 1
+----> 1 for batch in dataloader:
+      2     data, labels = batch
+      3     print(data, labels)
 
-# CSV 파일 로드 및 데이터 필터링
-class CustomDataset(Dataset):
-    def __init__(self, csv_file):
-        self.data = pd.read_csv(csv_file)
-        self.filtered_data = self.data[self.data['step_name'] == 'Depo']
-        self.labels = self.filtered_data['sfqr_afs2'].values
-        self.features = self.filtered_data.drop(columns=['sfqr_afs2', 'step_name']).values
+File c:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\torch\utils\data\dataloader.py:631, in _BaseDataLoaderIter.__next__(self)
+    628 if self._sampler_iter is None:
+    629     # TODO(https://github.com/pytorch/pytorch/issues/76750)
+    630     self._reset()  # type: ignore[call-arg]
+--> 631 data = self._next_data()
+    632 self._num_yielded += 1
+    633 if self._dataset_kind == _DatasetKind.Iterable and \
+    634         self._IterableDataset_len_called is not None and \
+    635         self._num_yielded > self._IterableDataset_len_called:
 
-    def __len__(self):
-        return len(self.filtered_data)
+File c:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\torch\utils\data\dataloader.py:675, in _SingleProcessDataLoaderIter._next_data(self)
+    673 def _next_data(self):
+    674     index = self._next_index()  # may raise StopIteration
+--> 675     data = self._dataset_fetcher.fetch(index)  # may raise StopIteration
+    676     if self._pin_memory:
+    677         data = _utils.pin_memory.pin_memory(data, self._pin_memory_device)
 
-    def __getitem__(self, idx):
-        return torch.tensor(self.features[idx], dtype=torch.float32), torch.tensor(self.labels[idx], dtype=torch.float32)
+File c:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\torch\utils\data\_utils\fetch.py:51, in _MapDatasetFetcher.fetch(self, possibly_batched_index)
+...
+Cell In[1], line 17
+     16 def __getitem__(self, idx):
+---> 17     return torch.tensor(self.features[idx], dtype=torch.float32), torch.tensor(self.labels[idx], dtype=torch.float32)
 
-# 데이터셋과 DataLoader 생성
-def create_dataloader(csv_file, batch_size):
-    dataset = CustomDataset(csv_file)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    return dataloader
+TypeError: must be real number, not str
