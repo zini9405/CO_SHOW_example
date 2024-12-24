cd391af1c0de20112f12410543201757eb3616cf
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2024-12-24 17:09:01 +0900

    s.py 업데이트

--- s.py
+++ s.py
@@ -1,6 +1,10 @@
-OSError: You are trying to access a gated repo.
-Make sure to have access to it at https://huggingface.co/meta-llama/Meta-Llama-3-8B-Instruct.
-403 Client Error. (Request ID: Root=1-676a5a37-7eb0488b0b98d76d4bc67a91;02510c6a-1d63-440e-9e35-87d2d1759e85)
-
-Cannot access gated repo for url https://huggingface.co/meta-llama/Meta-Llama-3-8B-Instruct/resolve/main/config.json.
-Your request to access model meta-llama/Meta-Llama-3-8B-Instruct is awaiting a review from the repo authors.
+from huggingface_hub import login
+import os
+#read
+# os.environ["HUGGING_FACE_HUB_TOKEN"] = "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"
+# write
+# os.environ["HUGGING_FACE_HUB_TOKEN"] = "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"
+os.environ["HUGGING_FACE_HUB_TOKEN"] = "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"
+os.environ['HF_HUB_ENABLE_HF_TRANSFER'] = "1"
+access_token = os.environ["HUGGING_FACE_HUB_TOKEN"]
+login(token=access_token)
