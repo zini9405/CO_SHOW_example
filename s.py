bc80a61d5505c1c2139cbe71d8a1209195051d29
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2024-12-24 19:08:42 +0900

    Update s.py

--- s.py
+++ s.py
@@ -1,16 +1,1 @@
-Traceback (most recent call last):
-  File "/raid/lee/anaconda3/envs/2024/bin/llamafactory-cli", line 5, in <module>
-    from llamafactory.cli import main
-  File "/raid/lee/workspace/sk/LLM/LLaMA-Factory/src/llamafactory/__init__.py", line 44, in <module>
-    from .extras.env import VERSION
-  File "/raid/lee/workspace/sk/LLM/LLaMA-Factory/src/llamafactory/extras/env.py", line 20, in <module>
-    import accelerate
-  File "/raid/lee/anaconda3/envs/2024/lib/python3.11/site-packages/accelerate/__init__.py", line 16, in <module>
-    from .accelerator import Accelerator
-  File "/raid/lee/anaconda3/envs/2024/lib/python3.11/site-packages/accelerate/accelerator.py", line 33, in <module>
-    import torch.utils.hooks as hooks
-  File "/raid/lee/anaconda3/envs/2024/lib/python3.11/site-packages/torch/utils/__init__.py", line 8, in <module>
-    from torch.utils import (
-  File "/raid/lee/anaconda3/envs/2024/lib/python3.11/site-packages/torch/utils/backcompat/__init__.py", line 2, in <module>
-    from torch._C import _set_backcompat_broadcast_warn
-ModuleNotFoundError: No module named 'torch._C'
+MAIN_RUNTIME	DD_USE_NUM	SLURRY_USE_NUM	PAD_COUNT	CARRIER_MTL_USE_NUM
