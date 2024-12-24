51a85f46ee84b55f280c8dfcf506fa09b370749d
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2024-12-24 15:53:45 +0900

    Update s.py

--- s.py
+++ s.py
@@ -1,1 +1,6 @@
-requests.exceptions.SSLError: (MaxRetryError("HTTPSConnectionPool(host='huggingface.co', port=443): Max retries exceeded with url: /meta-llama/Meta-Llama-3-8B-Instruct/resolve/main/config.json (Caused by SSLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: self-signed certificate in certificate chain (_ssl.c:1000)')))"), '(Request ID: 3cec945d-2dc6-4208-af16-207d068a98a3)')
+OSError: You are trying to access a gated repo.
+Make sure to have access to it at https://huggingface.co/meta-llama/Meta-Llama-3-8B-Instruct.
+403 Client Error. (Request ID: Root=1-676a5a37-7eb0488b0b98d76d4bc67a91;02510c6a-1d63-440e-9e35-87d2d1759e85)
+
+Cannot access gated repo for url https://huggingface.co/meta-llama/Meta-Llama-3-8B-Instruct/resolve/main/config.json.
+Your request to access model meta-llama/Meta-Llama-3-8B-Instruct is awaiting a review from the repo authors.
