f449c7df8ae89398d5c6e0d0488cbc91f1b7b673
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2024-12-24 14:11:37 +0900

    Update s.py

--- s.py
+++ s.py
@@ -1,3 +1,6 @@
-https://devocean.sk.com/blog/techBoardDetail.do?ID=165703
+df = data[['MAIN_RUNTIME', 'DD_USE_NUM', 'SLURRY_USE_NUM', 'PAD_COUNT', 'CARRIER_MTL_USE_NUM', 'GBIR']]
 
-http://155.230.135.187:15000/lab/tree/workspace/LG_pro/1st.ipynb
+
+
+quantile_transformer = QuantileTransformer(output_distribution='normal', random_state=42)
+self.quantile_transformer.fit_transform(self.labels.reshape(-1, 1)).flatten()
