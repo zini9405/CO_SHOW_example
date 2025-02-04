6fabf52ec0954e9d8e2a50595b91e02e825bf331
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-04 13:08:09 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -7,253 +7,249 @@
     }
   },
   "data": {
-    "name": "data-6815f624896541b2a2909d04a2390caf"
+    "name": "data-8c8648828e53f9fe0e4fbabb05b3b059"
   },
   "datasets": {
-    "data-6815f624896541b2a2909d04a2390caf": [
+    "data-8c8648828e53f9fe0e4fbabb05b3b059": [
+      {
+        "EQP_ID_MODULE_NAME": "CENC43B",
+        "ZDDFRONTMEAN_01_AFS2": -43.11592305780236
+      },
       {
         "EQP_ID_MODULE_NAME": "CENC43A",
-        "ZDDFRONTMEAN_01_AFS2": -26.667013163296872
+        "ZDDFRONTMEAN_01_AFS2": -26.499877216730944
       },
       {
         "EQP_ID_MODULE_NAME": "CENC36B",
-        "ZDDFRONTMEAN_01_AFS2": -18.410300910877517
+        "ZDDFRONTMEAN_01_AFS2": -22.075786425698926
       },
       {
         "EQP_ID_MODULE_NAME": "CENC9A",
-        "ZDDFRONTMEAN_01_AFS2": -18.200619344908514
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC15A",
-        "ZDDFRONTMEAN_01_AFS2": -17.273202883670226
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC34B",
-        "ZDDFRONTMEAN_01_AFS2": -17.11089591915571
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC8A",
-        "ZDDFRONTMEAN_01_AFS2": -16.992087451344133
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC41B",
-        "ZDDFRONTMEAN_01_AFS2": -16.458501306818658
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC48A",
-        "ZDDFRONTMEAN_01_AFS2": -16.31115690335134
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC46A",
-        "ZDDFRONTMEAN_01_AFS2": -16.20090955231714
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC44A",
-        "ZDDFRONTMEAN_01_AFS2": -15.769486370146652
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC47A",
-        "ZDDFRONTMEAN_01_AFS2": -15.442683883127607
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC35A",
-        "ZDDFRONTMEAN_01_AFS2": -15.37589912310407
+        "ZDDFRONTMEAN_01_AFS2": -18.667197462451576
       },
       {
         "EQP_ID_MODULE_NAME": "CENC5B",
-        "ZDDFRONTMEAN_01_AFS2": -15.135772292158444
+        "ZDDFRONTMEAN_01_AFS2": -17.734439336400133
       },
       {
-        "EQP_ID_MODULE_NAME": "CENC41A",
-        "ZDDFRONTMEAN_01_AFS2": -14.954028510990387
+        "EQP_ID_MODULE_NAME": "CENC8A",
+        "ZDDFRONTMEAN_01_AFS2": -17.10573899563486
       },
       {
-        "EQP_ID_MODULE_NAME": "CENC9B",
-        "ZDDFRONTMEAN_01_AFS2": -14.72429448905868
+        "EQP_ID_MODULE_NAME": "CENC48A",
+        "ZDDFRONTMEAN_01_AFS2": -16.914906655558678
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC44A",
+        "ZDDFRONTMEAN_01_AFS2": -16.752620551623835
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC15A",
+        "ZDDFRONTMEAN_01_AFS2": -16.409897370824094
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC46A",
+        "ZDDFRONTMEAN_01_AFS2": -16.38229625072584
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC41B",
+        "ZDDFRONTMEAN_01_AFS2": -15.796118297372217
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC34B",
+        "ZDDFRONTMEAN_01_AFS2": -15.464822341225583
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC47A",
+        "ZDDFRONTMEAN_01_AFS2": -15.22292881103986
       },
       {
         "EQP_ID_MODULE_NAME": "CENC42B",
-        "ZDDFRONTMEAN_01_AFS2": -14.21321760484958
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC34A",
-        "ZDDFRONTMEAN_01_AFS2": -14.114803474599864
+        "ZDDFRONTMEAN_01_AFS2": -14.655173228765987
       },
       {
         "EQP_ID_MODULE_NAME": "CENC16B",
-        "ZDDFRONTMEAN_01_AFS2": -13.11657670658612
+        "ZDDFRONTMEAN_01_AFS2": -13.78770556434004
       },
       {
-        "EQP_ID_MODULE_NAME": "CENC43B",
-        "ZDDFRONTMEAN_01_AFS2": -12.515292487954735
+        "EQP_ID_MODULE_NAME": "CENC41A",
+        "ZDDFRONTMEAN_01_AFS2": -13.596750250742517
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC34A",
+        "ZDDFRONTMEAN_01_AFS2": -13.53075581979755
       },
       {
         "EQP_ID_MODULE_NAME": "CENC15B",
-        "ZDDFRONTMEAN_01_AFS2": -12.44895673881785
+        "ZDDFRONTMEAN_01_AFS2": -13.347909102859632
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC9B",
+        "ZDDFRONTMEAN_01_AFS2": -13.327175253451339
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC35A",
+        "ZDDFRONTMEAN_01_AFS2": -13.143451538631632
       },
       {
         "EQP_ID_MODULE_NAME": "CENC5A",
-        "ZDDFRONTMEAN_01_AFS2": -12.057821472142496
+        "ZDDFRONTMEAN_01_AFS2": -12.929979433078671
       },
       {
         "EQP_ID_MODULE_NAME": "CENC48B",
-        "ZDDFRONTMEAN_01_AFS2": -11.268727996304612
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC11B",
-        "ZDDFRONTMEAN_01_AFS2": -10.80875547480021
+        "ZDDFRONTMEAN_01_AFS2": -11.687083544022444
       },
       {
         "EQP_ID_MODULE_NAME": "ZCENC01B",
-        "ZDDFRONTMEAN_01_AFS2": -10.566952191574746
+        "ZDDFRONTMEAN_01_AFS2": -10.982897954003997
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC11B",
+        "ZDDFRONTMEAN_01_AFS2": -10.056218735640995
       },
       {
         "EQP_ID_MODULE_NAME": "CENC47B",
-        "ZDDFRONTMEAN_01_AFS2": -10.398888635019029
-      },
-      {
-        "EQP_ID_MODULE_NAME": "ZCENC01A",
-        "ZDDFRONTMEAN_01_AFS2": -9.79869077729824
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC35B",
-        "ZDDFRONTMEAN_01_AFS2": -9.670804381271049
+        "ZDDFRONTMEAN_01_AFS2": -9.95696636382068
       },
       {
         "EQP_ID_MODULE_NAME": "CENC45B",
-        "ZDDFRONTMEAN_01_AFS2": -9.325795854614924
+        "ZDDFRONTMEAN_01_AFS2": -9.949346512459497
       },
       {
-        "EQP_ID_MODULE_NAME": "CENC8B",
-        "ZDDFRONTMEAN_01_AFS2": -8.452697613554799
+        "EQP_ID_MODULE_NAME": "ZCENC01A",
+        "ZDDFRONTMEAN_01_AFS2": -9.90198539587714
       },
       {
-        "EQP_ID_MODULE_NAME": "CENC17A",
-        "ZDDFRONTMEAN_01_AFS2": -8.23428089688932
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC17B",
-        "ZDDFRONTMEAN_01_AFS2": -8.166615063044102
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC44B",
-        "ZDDFRONTMEAN_01_AFS2": -7.899108214732119
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC45A",
-        "ZDDFRONTMEAN_01_AFS2": -7.2492563805034695
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC6B",
-        "ZDDFRONTMEAN_01_AFS2": -7.1482834522526995
+        "EQP_ID_MODULE_NAME": "CENC35B",
+        "ZDDFRONTMEAN_01_AFS2": -8.391756255249884
       },
       {
         "EQP_ID_MODULE_NAME": "CENC11A",
-        "ZDDFRONTMEAN_01_AFS2": -6.9209713154116015
+        "ZDDFRONTMEAN_01_AFS2": -8.251092499940006
       },
       {
-        "EQP_ID_MODULE_NAME": "CENC10A",
-        "ZDDFRONTMEAN_01_AFS2": -5.6768953673819444
+        "EQP_ID_MODULE_NAME": "CENC44B",
+        "ZDDFRONTMEAN_01_AFS2": -8.00439413095966
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC8B",
+        "ZDDFRONTMEAN_01_AFS2": -7.928842111156288
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC45A",
+        "ZDDFRONTMEAN_01_AFS2": -7.6014917716395205
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC17A",
+        "ZDDFRONTMEAN_01_AFS2": -7.582112408488526
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC17B",
+        "ZDDFRONTMEAN_01_AFS2": -6.948541415954172
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC6B",
+        "ZDDFRONTMEAN_01_AFS2": -5.8471556968987
       },
       {
         "EQP_ID_MODULE_NAME": "CENC46B",
-        "ZDDFRONTMEAN_01_AFS2": -4.913291137153545
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC16A",
-        "ZDDFRONTMEAN_01_AFS2": -3.3521171422453797
+        "ZDDFRONTMEAN_01_AFS2": -4.968255811178457
       },
       {
         "EQP_ID_MODULE_NAME": "ZCENC03A",
-        "ZDDFRONTMEAN_01_AFS2": -3.1569714939703433
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC7A",
-        "ZDDFRONTMEAN_01_AFS2": -2.5882515643065998
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC13B",
-        "ZDDFRONTMEAN_01_AFS2": -2.2432317795946006
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC7B",
-        "ZDDFRONTMEAN_01_AFS2": -1.6204388246844545
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC33A",
-        "ZDDFRONTMEAN_01_AFS2": -0.7172546502143338
+        "ZDDFRONTMEAN_01_AFS2": -3.561785581235171
       },
       {
         "EQP_ID_MODULE_NAME": "ZCENC02A",
-        "ZDDFRONTMEAN_01_AFS2": -0.4494770798475071
+        "ZDDFRONTMEAN_01_AFS2": -3.5013482966395566
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC10A",
+        "ZDDFRONTMEAN_01_AFS2": -3.446372618712549
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC16A",
+        "ZDDFRONTMEAN_01_AFS2": -3.021542188211128
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC7B",
+        "ZDDFRONTMEAN_01_AFS2": -2.6391176357540718
       },
       {
         "EQP_ID_MODULE_NAME": "ZCENC04B",
-        "ZDDFRONTMEAN_01_AFS2": -0.3877071880137679
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC31A",
-        "ZDDFRONTMEAN_01_AFS2": -0.38304351004864845
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC33B",
-        "ZDDFRONTMEAN_01_AFS2": -0.2821220359431262
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC12B",
-        "ZDDFRONTMEAN_01_AFS2": -0.21855379450582813
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC14A",
-        "ZDDFRONTMEAN_01_AFS2": -0.09264258634447206
+        "ZDDFRONTMEAN_01_AFS2": -2.3343252489968895
       },
       {
         "EQP_ID_MODULE_NAME": "ZCENC03B",
-        "ZDDFRONTMEAN_01_AFS2": 0.7143215349667653
+        "ZDDFRONTMEAN_01_AFS2": -2.1330044146801885
       },
       {
-        "EQP_ID_MODULE_NAME": "CENC36A",
-        "ZDDFRONTMEAN_01_AFS2": 1.5810620115686478
+        "EQP_ID_MODULE_NAME": "CENC12B",
+        "ZDDFRONTMEAN_01_AFS2": -1.1991222474282441
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC33B",
+        "ZDDFRONTMEAN_01_AFS2": -0.8964713389875907
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC13B",
+        "ZDDFRONTMEAN_01_AFS2": -0.7932932882556567
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC7A",
+        "ZDDFRONTMEAN_01_AFS2": -0.7520808179556803
       },
       {
         "EQP_ID_MODULE_NAME": "ZCENC02B",
-        "ZDDFRONTMEAN_01_AFS2": 1.6458178282702511
+        "ZDDFRONTMEAN_01_AFS2": -0.5737976558283543
       },
       {
-        "EQP_ID_MODULE_NAME": "CENC12A",
-        "ZDDFRONTMEAN_01_AFS2": 1.7212965773766016
+        "EQP_ID_MODULE_NAME": "CENC14A",
+        "ZDDFRONTMEAN_01_AFS2": -0.10690899997732685
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC33A",
+        "ZDDFRONTMEAN_01_AFS2": -0.09036792010063785
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC31A",
+        "ZDDFRONTMEAN_01_AFS2": 0.07788287781253131
       },
       {
         "EQP_ID_MODULE_NAME": "ZCENC04A",
-        "ZDDFRONTMEAN_01_AFS2": 2.3432329336981432
+        "ZDDFRONTMEAN_01_AFS2": 0.5542833036309559
       },
       {
-        "EQP_ID_MODULE_NAME": "CENC42A",
-        "ZDDFRONTMEAN_01_AFS2": 6.700602938166127
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC31B",
-        "ZDDFRONTMEAN_01_AFS2": 7.143021923762531
+        "EQP_ID_MODULE_NAME": "CENC12A",
+        "ZDDFRONTMEAN_01_AFS2": 5.2911770992712155
       },
       {
         "EQP_ID_MODULE_NAME": "CENC6A",
-        "ZDDFRONTMEAN_01_AFS2": 15.4337460643269
+        "ZDDFRONTMEAN_01_AFS2": 13.417068324585202
       },
       {
         "EQP_ID_MODULE_NAME": "CENC14B",
-        "ZDDFRONTMEAN_01_AFS2": 15.76348371700865
-      },
-      {
-        "EQP_ID_MODULE_NAME": "CENC10B",
-        "ZDDFRONTMEAN_01_AFS2": 20.34464143806814
+        "ZDDFRONTMEAN_01_AFS2": 16.294942645503305
       },
       {
         "EQP_ID_MODULE_NAME": "CENC13A",
-        "ZDDFRONTMEAN_01_AFS2": 20.80235299577677
+        "ZDDFRONTMEAN_01_AFS2": 20.99067460805711
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC31B",
+        "ZDDFRONTMEAN_01_AFS2": 24.151334257251964
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC42A",
+        "ZDDFRONTMEAN_01_AFS2": 25.469084854107262
+      },
+      {
+        "EQP_ID_MODULE_NAME": "CENC10B",
+        "ZDDFRONTMEAN_01_AFS2": 27.783735621259257
       },
       {
         "EQP_ID_MODULE_NAME": "CENC32A",
-        "ZDDFRONTMEAN_01_AFS2": 30.13336696244263
+        "ZDDFRONTMEAN_01_AFS2": 29.479954480107835
       }
     ]
   },
@@ -269,48 +265,11 @@
       "scale": {
         "clamp": false,
         "domain": [
-          -26.667013163296872,
-          30.13336696244263
+          -43.11592305780236,
+          29.479954480107835
         ]
       },
       "title": "ZDD",
-      "title": "ZDD",
-      "type": "quantitative"
-      "title": "ZDD",
-      "type": "quantitative"
-      "title": "ZDD",
-      "type": "quantitative"
-    }
-  },
-      "title": "ZDD",
-      "type": "quantitative"
-    }
-      "title": "ZDD",
-      "type": "quantitative"
-      "title": "ZDD",
-      "title": "ZDD",
-      "type": "quantitative"
-      "title": "ZDD",
-      "title": "ZDD",
-      "type": "quantitative"
-      "title": "ZDD",
-      "title": "ZDD",
-      "type": "quantitative"
-      "title": "ZDD",
-      "title": "ZDD",
-      "type": "quantitative"
-      "title": "ZDD",
-      "title": "ZDD",
-      "type": "quantitative"
-    }
-      "title": "ZDD",
-      "type": "quantitative"
-      "title": "ZDD",
-      "type": "quantitative"
-      "title": "ZDD",
-      "title": "ZDD",
-      "title": "ZDD",
-      "type": "quantitative"
       "type": "quantitative"
     }
   },
@@ -319,3 +278,82 @@
     "type": "bar"
   }
 }
+
+
+import numpy as np
+import pandas as pd
+import altair as alt
+import streamlit as st
+import matplotlib.pyplot as plt
+import datetime
+from glob import glob
+from src.process import to_dict, get_golden_tool, to_dict_
+from src.graph import plot_cluster, plot_pie_chart, get_color, plot_shape
+#from src.graph import *
+from src.misc import mode, get_fname
+import os
+
+if 'df_Y' not in st.session_state:
+    df_Y = pd.read_csv('./asset/wire_saw_summary/sfqr/all_minmax_with_predictions_and_importance.csv', low_memory=False)
+    df_Y['HST_REG_DTTM'] = df_Y['HST_REG_DTTM'].map(lambda x: datetime.datetime.strptime(str(x), '%Y-%m-%d %H:%M:%S').date())
+    st.session_state['df_Y'] = df_Y
+    
+if 'df_cluster' not in st.session_state:
+    st.session_state['df_cluster'] = pd.read_csv('./asset/wire_saw_summary/sfqr/embedding_results.csv')
+
+if 'list_recipe' not in st.session_state:
+    st.session_state['list_recipe'] = st.session_state['df_Y'].RECIPE_ID.unique().tolist()
+
+if 'dict_cluster' not in st.session_state:
+    st.session_state['dict_cluster'] = to_dict_(st.session_state['df_cluster'])
+
+st.set_page_config(
+    page_title = 'WIRE SAW - WARP',
+    page_icon = '📊',
+    initial_sidebar_state = 'collapsed',
+    layout = 'wide'
+)
+
+
+start_date = st.sidebar.date_input('Start date', datetime.date.today() - datetime.timedelta(days=60))
+end_date = st.sidebar.date_input('End date', datetime.date.today())
+
+list_lot_id = st.session_state['df_Y'][(st.session_state['df_Y'].HST_REG_DTTM.between(start_date, end_date, inclusive='both'))].WAF_ID.tolist()
+
+
+st.markdown('## EPI SFQR')
+
+# 1. warp trend
+st.markdown('---')
+st.markdown('### 1. SFQR 품질 현황')
+
+df_warp = st.session_state['df_Y'][st.session_state['df_Y'].WAF_ID.isin(list_lot_id)].groupby('EQP_ID_MODULE_NAME').ZDDFRONTMEAN_01_AFS2.mean().reset_index().sort_values('ZDDFRONTMEAN_01_AFS2')
+
+y_min = df_warp['ZDDFRONTMEAN_01_AFS2'].min()
+y_max = df_warp['ZDDFRONTMEAN_01_AFS2'].max()
+# print(y_min, y_max)
+
+
+
+chart = alt.Chart(df_warp).mark_bar(color = '#E1002A').encode(
+    x = alt.X('EQP_ID_MODULE_NAME', title = None, sort = '-y'),
+    y = alt.Y('ZDDFRONTMEAN_01_AFS2', title = 'ZDD', scale = alt.Scale(domain = [y_min, y_max], clamp = False))
+)
+
+json_data = chart.to_json()
+print(json_data)
+
+st.altair_chart(chart, use_container_width = True)
+
+
+SyntaxError: Unexpected token 'N', ..."domain": [NaN, NaN]}"... is not valid JSON
+at JSON.parse (<anonymous>)
+at E.generateSpec (http://localhost:20001/static/js/8460.570de48a.chunk.js:1:7100)
+at E.createView (http://localhost:20001/static/js/8460.570de48a.chunk.js:1:11660)
+at E.componentDidMount (http://localhost:20001/static/js/8460.570de48a.chunk.js:1:9945)
+at yl (http://localhost:20001/static/js/main.dbbac55a.js:10:2321028)
+at zl (http://localhost:20001/static/js/main.dbbac55a.js:10:2320801)
+at gl (http://localhost:20001/static/js/main.dbbac55a.js:10:2320337)
+at http://localhost:20001/static/js/main.dbbac55a.js:10:2332174
+at Ac (http://localhost:20001/static/js/main.dbbac55a.js:10:2332689)
+at cc (http://localhost:20001/static/js/main.dbbac55a.js:10:2326408)
