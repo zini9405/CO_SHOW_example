9daf51db02a141b37a88c0b83aa87a771cd135b2
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-05 16:34:00 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,1 +1,7 @@
-st.markdown("<h2 style='text-align: center;'>📊 EQP_1 vs EQP_2 - X 값 비교</h2>", unsafe_allow_html=True)
+EQP_ID_MODULE_NAME 선택 - HST_REG_DTTM 선택(동일한 날짜는 순서대로 점으로 표시) - WAF_ID 선택하면 ZDDFRONTMEAN_01_AFS2값과 pred값 두개 비교하는 그래프 그려줘.
+
+
+  if 'df_Y' not in st.session_state:
+    df_Y = pd.read_csv('./asset/wire_saw_summary/sfqr/all_minmax_with_predictions_and_importance.csv', low_memory=False)
+    df_Y['HST_REG_DTTM'] = df_Y['HST_REG_DTTM'].map(lambda x: datetime.datetime.strptime(str(x), '%Y-%m-%d %H:%M:%S').date())
+    st.session_state['df_Y'] = df_Y
