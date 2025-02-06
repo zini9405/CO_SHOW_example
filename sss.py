33fef5ac7d717d98f508363d117714ec00c93736
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-06 10:58:16 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,79 +1,85 @@
-import streamlit as st
-import pandas as pd
-import altair as alt
+import os
 import datetime
+import hashlib
+import pandas as pd
+from glob import glob
+from tqdm import tqdm
+import numpy as np
+from datetime import datetime, timedelta
+from pathlib import Path
+import paramiko
+import shutil
+from argparse import ArgumentParser
+import logging
+import sys
+from scipy import interpolate as scipy_interpolate
 
-# Streamlit 페이지 설정
-st.set_page_config(
-    page_title="EQP 데이터 비교",
-    page_icon="📊",
-    layout="wide"
-)
+import torch
+import numpy as np
+# from umap import UMAP
 
-# 데이터 로드
-if 'df_Y' not in st.session_state:
-    df_Y = pd.read_csv('./asset/wire_saw_summary/sfqr/all_minmax_with_predictions_and_importance.csv', low_memory=False)
-    df_Y['HST_REG_DTTM'] = pd.to_datetime(df_Y['HST_REG_DTTM']).dt.strftime('%Y-%m-%d')  # 날짜 포맷 변환
-    st.session_state['df_Y'] = df_Y
+# from src.tables import eqp_table
+# from src.inference import Inference
 
-df_Y = st.session_state['df_Y']
+# from src.misc import set_dir, mode, get_fname, mode, set_dir, save_json, load_json
 
-# Streamlit UI 제목
-st.markdown("<h2 style='text-align: center;'>📊 EQP 데이터 비교 (ZDDFRONTMEAN_01_AFS2 vs Pred)</h2>", unsafe_allow_html=True)
+# from src.build import read_data_file
+# from src.datas import TrainDataset
+from torch.utils.data import DataLoader
+# from src.models import get_attn
+# from src.misc import load_yaml
+# from src.build import *
 
-# EQP_ID_MODULE_NAME 선택
-eqp_list = df_Y['EQP_ID_MODULE_NAME'].unique()
-selected_eqp = st.sidebar.selectbox("EQP_ID_MODULE_NAME 선택", eqp_list)
+import warnings
+warnings.filterwarnings(action='ignore')
 
-# 선택한 EQP에 해당하는 데이터 필터링
-df_filtered_eqp = df_Y[df_Y['EQP_ID_MODULE_NAME'] == selected_eqp]
 
-# HST_REG_DTTM 선택 (해당 EQP에서 날짜 목록만 가져오기)
-date_list = df_filtered_eqp['HST_REG_DTTM'].unique()
-selected_date = st.sidebar.selectbox("HST_REG_DTTM 선택", date_list)
 
-# 선택한 날짜의 데이터 필터링
-df_filtered_date = df_filtered_eqp[df_filtered_eqp['HST_REG_DTTM'] == selected_date]
 
-# ✅ 전체 WAF_ID 데이터를 **수평 정렬된 그래프**로 표시
-df_melted_all = df_filtered_date.melt(id_vars=['WAF_ID'], value_vars=['ZDDFRONTMEAN_01_AFS2', 'pred'],
-                                      var_name='Variable', value_name='Value')
+parser = ArgumentParser()
+parser.add_argument('--date', type=str)
+args = parser.parse_args()
 
-# 📊 WAF_ID별 그래프 (X축을 WAF_ID로 설정하여 수평 정렬)
-chart_all = alt.Chart(df_melted_all).mark_circle(size=80).encode(
-    x=alt.X('WAF_ID:N', title="WAF_ID", sort='ascending'),
-    y=alt.Y('Value:Q', title="값"),
-    color=alt.Color('Variable:N', title="변수 종류"),
-    tooltip=['WAF_ID', 'Variable', 'Value']
-).properties(title=f'📊 {selected_date} - 전체 WAF_ID 데이터')
 
-# 그래프 출력
-st.altair_chart(chart_all, use_container_width=True)
+today_date = datetime.today().strftime("%Y_%m_%d")[2:]
+check_last_month = False
 
-# ✅ Streamlit selectbox로 WAF_ID 선택
-selected_waf = st.sidebar.selectbox("WAF_ID 선택", df_filtered_date['WAF_ID'].unique())
+if int(today_date[-2:]) < 7:
+    check_last_month = True
 
-# 선택한 WAF_ID 데이터 필터링
-df_final = df_filtered_date[df_filtered_date['WAF_ID'] == selected_waf]
+#####################################################
+ #   Get X filenames
+#####################################################
+host = "10.150.9.121" 
+port = 22 
+SSH_Client = paramiko.SSHClient()
+# transprot = paramiko.transport.Transport(host,port)
+userId = "sksl_ds02"  # example
+password = 'sksl_ds02!' # example
+SSH_Client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+SSH_Client.connect(hostname=host,
+                port=port,
+                username=userId,
+                password=password,
+                look_for_keys=False)
+sftp_client = SSH_Client.open_sftp()
 
-# 데이터 확인 및 개별 그래프 생성
-if df_final.empty:
-    st.warning("선택한 WAF_ID에 대한 데이터가 없습니다.")
-else:
-    # ✅ 📌 Altair 시각화를 위한 데이터 변환
-    df_melted = df_final.melt(id_vars=['HST_REG_DTTM'], value_vars=['ZDDFRONTMEAN_01_AFS2', 'pred'], var_name='Variable', value_name='Value')
 
-    # ✅ 📊 선택한 WAF_ID의 상세 그래프
-    chart = alt.Chart(df_melted).mark_line(point=True).encode(
-        x=alt.X('HST_REG_DTTM:T', title="날짜"),
-        y=alt.Y('Value:Q', title="값"),
-        color=alt.Color('Variable:N', title="변수 종류"),
-        tooltip=['HST_REG_DTTM', 'Variable', 'Value']
-    ).properties(title=f'📊 {selected_waf} - ZDDFRONTMEAN_01_AFS2 vs Pred')
+remote_x_path = f'/home/sksl_ds02/ANALYSIS_DATA_SET/X_TEMP/3200_WIRE_SAW/{today_date[:5]}'
+x_dir_list = sftp_client.listdir(remote_x_path)
 
-    # 개별 그래프 출력
-    st.altair_chart(chart, use_container_width=True)
+if check_last_month:
+    last_month = datetime.strptime(today_date, '%y_%m_%d') - timedelta(weeks=1)
+    last_month = last_month.strftime('%y_%m_%d')
+    remote_x_path = f'/home/sksl_ds02/ANALYSIS_DATA_SET/X_TEMP/3200_WIRE_SAW/{last_month[:5]}'
+    x_dir_list.extend(sftp_client.listdir(remote_x_path))
 
-    # ✅ 📋 선택한 WAF_ID의 데이터 테이블
-    st.markdown(f"### 📋 {selected_waf}의 상세 데이터")
-    st.dataframe(df_final)
+sftp_client.close()
+SSH_Client.close() 
+
+
+usage: ipykernel_launcher.py [-h] [--date DATE]
+ipykernel_launcher.py: error: unrecognized arguments: --f=c:\Users\SKsiltron\AppData\Roaming\jupyter\runtime\kernel-v324ab0c7b101ffde14d9a9966ff050e95fea44d1c.json
+An exception has occurred, use %tb to see the full traceback.
+
+SystemExit: 2
