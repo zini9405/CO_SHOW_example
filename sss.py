e30ae1df14139b90f75062205820943be5339ff0
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-10 15:41:41 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,2 +1,2 @@
-streamlit.errors.StreamlitAPIException: Could not find page: `./pages_ZDD/ZDD.py`. Must be the file path relative to the main script, from the directory: `smart-tttm_j`. Only the main app 
+streamlit.errors.StreamlitAPIException: Could not find page: `./pages/ZDD/ZDD.py`. Must be the file path relative to the main script, from the directory: `smart-tttm_j`. Only the main app 
 file and files in the `pages/` directory are supported.
