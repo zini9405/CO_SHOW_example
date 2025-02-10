825b457def1480b65ba125adf4d6253350b91db6
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-10 15:45:17 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,2 +1,27 @@
-streamlit.errors.StreamlitAPIException: Could not find page: `./pages/ZDD/ZDD.py`. Must be the file path relative to the main script, from the directory: `smart-tttm_j`. Only the main app 
-file and files in the `pages/` directory are supported.
+with col34:
+    is_SFQR_clicked = st.button(
+        label = 'SFQR', 
+        use_container_width = True
+    )
+    if is_SFQR_clicked:
+        st.switch_page(f'./pages/SFQR.py')
+
+    is_ZDD_clicked = st.button(
+        label = 'ZDD', 
+        use_container_width = True
+    )
+    if is_ZDD_clicked:
+        st.switch_page(f'./pages/ZDD.py')
+
+    st.link_button(
+        label = 'SITE NT', 
+        url = '', 
+        use_container_width = True, 
+        disabled = True
+    )
+    st.link_button(
+        label = 'DELTA TEMP', 
+        url = '', 
+        use_container_width = True, 
+        disabled = True
+    )
