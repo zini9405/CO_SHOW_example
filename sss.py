d82934fcfd7d459fbf5884856d2ff10273b4e7db
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-12 11:43:14 +0900

    sss.py 업데이트

--- sss.py
+++ sss.py
@@ -1,12 +1,31 @@
-y_6300 = '6300_y_24_01/24_01_01_6300_6350.parquet' WAFER_ID, GBIR_AFS2
-y_6900 = '6900_24_01/24_01_01_6900_6950.parquet' WAFER_ID, GBIR_AFS2
-x_6300 = '24_01_x/24_01_01_6100_DSP.parquet' WAF_ID
-A = GBIR_20240110_A동_Sheet1.csv WAF_ID
-b = GBIR_20240110_B동_Sheet1.csv WAF_ID
+import pandas as pd
 
+# 파일 불러오기
+y_6300 = pd.read_parquet('6300_y_24_01/24_01_01_6300_6350.parquet', columns=['WAFER_ID', 'GBIR_AFS2'])
+y_6900 = pd.read_parquet('6900_24_01/24_01_01_6900_6950.parquet', columns=['WAFER_ID', 'GBIR_AFS2'])
+x_6300 = pd.read_parquet('24_01_x/24_01_01_6100_DSP.parquet')
+A = pd.read_csv('GBIR_20240110_A동_Sheet1.csv', usecols=['WAF_ID'])
+B = pd.read_csv('GBIR_20240110_B동_Sheet1.csv', usecols=['WAF_ID'])
 
-나는 x_6300 파일의 기준으로 5개의 데이터를 취합하고 싶어.
-x_6300의 WAF_ID와 동일한 y_6300의 WAFER_ID, y_6900의 WAFER_ID, A의 WAF_ID, B의 WAF_ID(단, 5개 WAF ID가 다 존재하는 것만 취합해줘)
-그리고 y_6300, y_6900 파일에는 GBIR_AFS2만 추출해서 x_6300에 취합해줘
-그리고  y_6900의 WAFER_ID에 해당하는 GBIR_AFS2는 6900_GBIR_AFS2로 이름 변경해주고  y_6300의 WAFER_ID에 해당하는 GBIR_AFS2는 6300_GBIR_AFS2로 이름 변경해줘
-중요한 데이터 전처리야. 신중하고 오류없이 처리해줘.
+# 컬럼 이름 변경
+y_6300.rename(columns={'GBIR_AFS2': '6300_GBIR_AFS2'}, inplace=True)
+y_6900.rename(columns={'GBIR_AFS2': '6900_GBIR_AFS2'}, inplace=True)
+
+# 컬럼명 통일 (WAFER_ID → WAF_ID)
+y_6300.rename(columns={'WAFER_ID': 'WAF_ID'}, inplace=True)
+y_6900.rename(columns={'WAFER_ID': 'WAF_ID'}, inplace=True)
+
+# 5개 데이터셋을 기준으로 WAF_ID가 모두 존재하는 값만 선택
+merged_df = (
+    x_6300[['WAF_ID']]
+    .merge(y_6300, on='WAF_ID', how='inner')
+    .merge(y_6900, on='WAF_ID', how='inner')
+    .merge(A, on='WAF_ID', how='inner')
+    .merge(B, on='WAF_ID', how='inner')
+)
+
+# 결과 확인
+print(merged_df.head())
+
+# CSV 저장 (필요하면 주석 해제)
+# merged_df.to_csv('merged_data.csv', index=False)
