7af1615e5305ab5c07bbf434a95849ebd5611dca
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-12 19:39:20 +0900

    sss.py 업데이트

--- sss.py
+++ sss.py
@@ -3,11 +3,11 @@ import pandas as pd
 # CSV 파일 불러오기
 df = pd.read_csv('all.csv')
 
-# 각 EQP_ID_MODULE_NAME 별 고유 RECIPE_ID 개수 계산
-eqp_recipe_counts = df.groupby('EQP_ID_MODULE_NAME')['RECIPE_ID'].nunique()
+# 각 EQP_ID_MODULE_NAME별 각 RECIPE_ID의 개수 계산
+eqp_recipe_counts = df.groupby(['EQP_ID_MODULE_NAME', 'RECIPE_ID']).size().reset_index(name='COUNT')
 
 # 결과 출력
 print(eqp_recipe_counts)
 
 # CSV로 저장 (필요하면 주석 해제)
-# eqp_recipe_counts.to_csv('eqp_recipe_counts.csv', header=True)
+# eqp_recipe_counts.to_csv('eqp_recipe_counts.csv', index=False)
