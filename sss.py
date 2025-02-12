daa4c1ace313c4141d4e922c326ed2cfb1722a99
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-12 19:29:17 +0900

    sss.py 업데이트

--- sss.py
+++ sss.py
@@ -3,11 +3,11 @@ import pandas as pd
 # CSV 파일 불러오기
 df = pd.read_csv('all.csv')
 
-# EQP_ID_MODULE_NAME 열의 고유값 및 개수 계산
-eqp_counts = df['EQP_ID_MODULE_NAME'].value_counts()
+# 각 EQP_ID_MODULE_NAME 별 고유 RECIPE_ID 개수 계산
+eqp_recipe_counts = df.groupby('EQP_ID_MODULE_NAME')['RECIPE_ID'].nunique()
 
 # 결과 출력
-print(eqp_counts)
+print(eqp_recipe_counts)
 
 # CSV로 저장 (필요하면 주석 해제)
-# eqp_counts.to_csv('eqp_id_module_name_counts.csv', header=True)
+# eqp_recipe_counts.to_csv('eqp_recipe_counts.csv', header=True)
