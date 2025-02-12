0750f818423e30b32c767d7a6e4c3c369a1993dd
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-12 19:21:50 +0900

    sss.py 업데이트

--- sss.py
+++ sss.py
@@ -1,1 +1,13 @@
-all.csv 파일에 EQP_ID_MODULE_NAME 열이 있어. set을 사용해 각 EQP_ID_MODULE_NAME에 대한 갯수가 얼마나 있는지 알수있는 코드 구현해줘
+import pandas as pd
+
+# CSV 파일 불러오기
+df = pd.read_csv('all.csv')
+
+# EQP_ID_MODULE_NAME 열의 고유값 및 개수 계산
+eqp_counts = df['EQP_ID_MODULE_NAME'].value_counts()
+
+# 결과 출력
+print(eqp_counts)
+
+# CSV로 저장 (필요하면 주석 해제)
+# eqp_counts.to_csv('eqp_id_module_name_counts.csv', header=True)
