aa82f032dbdfe4adfac5f8ea95f293e94f63ff7c
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-18 18:26:52 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,13 +1,50 @@
-TypeError: plot_cluster_gbir() got an unexpected keyword argument 'df_cluster_gbir'
-Traceback:
-File "C:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\streamlit\runtime\scriptrunner\exec_code.py", line 88, in exec_func_with_error_handling
-    result = func()
-             ^^^^^^
-File "C:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\streamlit\runtime\scriptrunner\script_runner.py", line 579, in code_to_exec
-    exec(code, module.__dict__)
-File "C:\Users\SKsiltron\Desktop\smart-tttm_j\main.py", line 29, in <module>
-    pg.run()
-File "C:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\streamlit\navigation\page.py", line 303, in run
-    exec(code, module.__dict__)
-File "C:\Users\SKsiltron\Desktop\smart-tttm_j\pages\GBIR.py", line 113, in <module>
-    fig = plot_cluster_gbir(
+def plot_cluster_gbir(df_cluster, eqp_1, eqp_2):
+    fig = plt.figure(figsize = (8, 8))
+    ax = fig.add_subplot()
+
+    title = ''
+
+    x_c = []
+    y_c = []
+
+    for i, row in df_cluster.iterrows():
+        eqp = row['EQP_ID']
+        warp = row['6900_GBIR_AFS2']
+
+        if eqp in [eqp_1, eqp_2]:
+            weight = 'bold'
+            color = 'b' if eqp == eqp_1 else 'r'
+            fontsize = 15
+            title += f'{eqp}: {warp:.2f}um. '
+            x_c.append(row['X'])
+            y_c.append(row['Y'])
+
+        else:
+            weight = None
+            color = 'gray'
+            fontsize = 10
+
+        ax.text(
+            x = row['X'], 
+            y = row['Y'], 
+            s = row['EQP_ID'], 
+            color = color, 
+            fontsize = fontsize, 
+            ha = 'center',
+            va = 'center',
+            weight = weight
+        )
+
+    ax.add_patch(plt.Circle(
+        xy = (np.mean(x_c), np.mean(y_c)), 
+        radius = 0.15,
+        fc = 'w',
+        ec = '#E1002A',
+        linestyle = '--',
+        linewidth = 2
+    ))
+
+    ax.set_title(title.strip(), fontsize = 15)
+    ax.set_xlim(-0.15, 1.15)
+    ax.set_ylim(-0.15, 1.15)
+    return fig
