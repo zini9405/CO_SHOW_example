1c7c7939501c6b9dc200c6510df3e090c64be2b7
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-18 18:28:18 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,50 +1,21 @@
-def plot_cluster_gbir(df_cluster, eqp_1, eqp_2):
-    fig = plt.figure(figsize = (8, 8))
-    ax = fig.add_subplot()
+print(st.session_state['df_cluster_gbir'])
+       EQP_ID         X         Y  6900_GBIR_AFS2
+0    BPDPD100  0.342670  0.216720        0.079025
+1    BPDPD101  0.516418  0.573984        0.101918
+2    BPDPD102  0.650096  0.297412        0.098439
+3    BPDPD103  0.702466  0.692311        0.076961
+4    BPDPD104  0.245268  0.610878        0.092579
+..        ...       ...       ...             ...
+121   TPDSP49  0.504799  1.000000        0.200366
+122   TPDSP51  0.716665  0.750337        0.152980
+123   TPDSP52  0.348907  0.469353        0.197178
+124   TPDSP55  0.497973  0.847248        0.170392
+125    TPDSP9  0.113013  0.662466        0.302143
 
-    title = ''
-
-    x_c = []
-    y_c = []
-
-    for i, row in df_cluster.iterrows():
-        eqp = row['EQP_ID']
-        warp = row['6900_GBIR_AFS2']
-
-        if eqp in [eqp_1, eqp_2]:
-            weight = 'bold'
-            color = 'b' if eqp == eqp_1 else 'r'
-            fontsize = 15
-            title += f'{eqp}: {warp:.2f}um. '
-            x_c.append(row['X'])
-            y_c.append(row['Y'])
-
-        else:
-            weight = None
-            color = 'gray'
-            fontsize = 10
-
-        ax.text(
-            x = row['X'], 
-            y = row['Y'], 
-            s = row['EQP_ID'], 
-            color = color, 
-            fontsize = fontsize, 
-            ha = 'center',
-            va = 'center',
-            weight = weight
-        )
-
-    ax.add_patch(plt.Circle(
-        xy = (np.mean(x_c), np.mean(y_c)), 
-        radius = 0.15,
-        fc = 'w',
-        ec = '#E1002A',
-        linestyle = '--',
-        linewidth = 2
-    ))
-
-    ax.set_title(title.strip(), fontsize = 15)
-    ax.set_xlim(-0.15, 1.15)
-    ax.set_ylim(-0.15, 1.15)
-    return fig
+with col3:
+    fig = plot_cluster_gbir(
+        df_cluster_gbir = st.session_state['df_cluster_gbir'], 
+        eqp_1 = eqp_1, 
+        eqp_2 = eqp_2
+    )
+    st.pyplot(fig, use_container_width = True)
