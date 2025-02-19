4d55a35cf6629ca44fb4cd4b428f7e6cfbb9b01e
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-19 13:14:23 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,21 +1,29 @@
-print(st.session_state['df_cluster_gbir'])
-       EQP_ID         X         Y  6900_GBIR_AFS2
-0    BPDPD100  0.342670  0.216720        0.079025
-1    BPDPD101  0.516418  0.573984        0.101918
-2    BPDPD102  0.650096  0.297412        0.098439
-3    BPDPD103  0.702466  0.692311        0.076961
-4    BPDPD104  0.245268  0.610878        0.092579
-..        ...       ...       ...             ...
-121   TPDSP49  0.504799  1.000000        0.200366
-122   TPDSP51  0.716665  0.750337        0.152980
-123   TPDSP52  0.348907  0.469353        0.197178
-124   TPDSP55  0.497973  0.847248        0.170392
-125    TPDSP9  0.113013  0.662466        0.302143
-
-with col3:
-    fig = plot_cluster_gbir(
-        df_cluster_gbir = st.session_state['df_cluster_gbir'], 
-        eqp_1 = eqp_1, 
-        eqp_2 = eqp_2
+with col34:
+    is_SFQR_clicked = st.button(
+        label = 'SFQR', 
+        use_container_width = True
     )
-    st.pyplot(fig, use_container_width = True)
+    if is_SFQR_clicked:
+        st.switch_page(f'./pages/SFQR.py')
+
+    is_ZDD_clicked = st.button(
+        label = 'ZDD', 
+        use_container_width = True
+    )
+    if is_ZDD_clicked:
+        st.switch_page(f'./pages/ZDD.py')
+
+
+- page
+-- SFQR.py
+-- SFQR_pred.py
+-- ZDD.py
+-- ZDD_pred.py
+
+
+SFQR을 클릭하고 SFQR_pred하면 파일이 작동이 돼.
+ZDD를 클릭학과 ZDD_pred하면 파일이 작동이 돼
+SFQR을 클릭하고 ZDD_pred하면 파일이 엉켜서 작동이 이상하게 돼.
+ZDD를 클릭학과 SFQR_pred하면 파일이 엉켜서 작동이 이상하게 돼.
+
+해결해줘.
