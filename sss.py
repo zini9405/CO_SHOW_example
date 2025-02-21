630c2c9da0b04007caaded9a94df2dcced4dcbc8
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-02-21 16:22:31 +0900

    sss.py 업데이트

--- sss.py
+++ sss.py
@@ -1,5 +1,8 @@
-# Train/Val Split
-def train_val_split(dataset, val_ratio=0.2):
+def train_val_split_sequential(dataset, val_ratio=0.2):
     val_size = int(len(dataset) * val_ratio)
     train_size = len(dataset) - val_size
-    return random_split(dataset, [train_size, val_size])
+    
+    train_dataset = dataset[:train_size]  # 앞부분을 train으로
+    val_dataset = dataset[train_size:]  # 뒷부분을 val로
+    
+    return train_dataset, val_dataset
