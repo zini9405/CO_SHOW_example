2574b0fb9c469c7af30466e3278c74eefa740183
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-03-05 17:12:00 +0900

    sss.py 업데이트

--- sss.py
+++ sss.py
@@ -6,8 +6,11 @@ df = pd.read_csv("x.csv")
 # '_Unnamed: 0'이 포함된 모든 열 삭제
 df = df.drop(columns=[col for col in df.columns if "_Unnamed: 0" in col], errors="ignore")
 
-# 값이 빈 값(NaN)인 열 삭제
-df = df.dropna(axis=1, how="all")  # 특정 열이 아니라, 전체 열 중 NaN만 포함된 열 삭제
+# 모든 값이 NaN인 열 삭제
+df = df.dropna(axis=1, how="all")
+
+# 열 값이 동일한 경우 중복된 열 삭제
+df = df.T.drop_duplicates().T  # Transpose하여 중복된 열을 찾고 다시 원래 형태로 변환
 
 # 결과 저장
 df.to_csv("cleaned_x.csv", index=False)
