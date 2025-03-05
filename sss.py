8a7948110b36f403e3da200af0466138d95e29ac
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-03-05 18:01:36 +0900

    sss.py 업데이트

--- sss.py
+++ sss.py
@@ -1,3 +1,19 @@
-LIFT-PIN1_PROD_ID열에 CP-MCLT, DUMMY-ALL, BULK-FE, ENGR-NIP 행 삭제
-LIFT-PIN2_PROD_ID열에 CP-MCLT, DUMMY-ALL, BULK-FE, ENGR-NIP 행 삭제
-LIFT-PIN3_PROD_ID열에 CP-MCLT, DUMMY-ALL, BULK-FE, ENGR-NIP 행 삭제
+import pandas as pd
+
+# CSV 파일 로드
+df = pd.read_csv("x.csv")
+
+# 삭제할 값 목록
+values_to_remove = ["CP-MCLT", "DUMMY-ALL", "BULK-FE", "ENGR-NIP"]
+
+# 특정 열에서 해당 값을 가진 행 삭제
+df = df[~df["LIFT-PIN1_PROD_ID"].isin(values_to_remove)]
+df = df[~df["LIFT-PIN2_PROD_ID"].isin(values_to_remove)]
+df = df[~df["LIFT-PIN3_PROD_ID"].isin(values_to_remove)]
+
+# 결과 저장
+df.to_csv("filtered_x.csv", index=False)
+
+# 변환된 데이터 출력
+import ace_tools as tools
+tools.display_dataframe_to_user(name="Filtered Data", dataframe=df)
