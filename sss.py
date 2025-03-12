ee8879af2c7bf54e3644dd5fbfd80ed76bb40429
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-03-12 13:43:34 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,15 +1,2 @@
----------------------------------------------------------------------------
-AttributeError                            Traceback (most recent call last)
-Cell In[20], line 162
-    158     print("Variable Importance:", importance_dict)
-    161 if __name__ == "__main__":
---> 162     main()
-
-Cell In[20], line 119
-    117 print(true_labels)
-    118 print(r2_score(true_labels, predictions))
---> 119 predictions = predictions.flattenen()
-    120 true_labels = true_labels.flattenen()
-    121 # 데이터프레임 생성
-
-AttributeError: 'numpy.ndarray' object has no attribute 'flattenen'
+[1, 4, 5 ,6 ] 되면 * 3하면
+[1, 1, 1, 4, 4, 4, 5, 5, 5 , 6, 6, 6 ]로 만드는 코드 구현
