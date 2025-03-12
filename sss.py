873558b155ed3a8e84079135966426d4b5be7826
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-03-12 10:53:29 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,71 +1,30 @@
----------------------------------------------------------------------------
-RuntimeError                              Traceback (most recent call last)
-Cell In[56], line 61
-     59 if __name__ == "__main__":
-     60     set_seed(42)
----> 61     main()
+MemoryError                               Traceback (most recent call last)
+Cell In[10], line 148
+    144     print("Variable Importance:", importance_dict)
+    147 if __name__ == "__main__":
+--> 148     main()
 
-Cell In[56], line 48
-     45 save_path = "model_weights_GBIR_25312.pth"
-     46 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
----> 48 train_model_inputs(
-     49     model=model,
-     50     train_loader=train_loader,
-     51     val_loader=val_loader,
-     52     criterion=nn.HuberLoss(delta=1.0),
-     53     optimizer=optimizer,
-     54     num_epochs=2000,
-     55     device=device,
-     56     save_path=save_path
-     57 )
+Cell In[10], line 101
+     98 predictions, true_labels, attention_scores = test_model(model, train_loader_, device, save_path, train_loader.quantile_transformer)
+    100 # 그래프 출력
+--> 101 correlation, predictions, true_labels = calculate_correlation(predictions, true_labels)
+    102 print(correlation)
+    103 print(predictions)
 
-Cell In[54], line 16
-     12 batch_features, batch_eqp_ids, batch_labels = zip(*batch_)
-     13 batch_features, batch_eqp_ids, batch_labels = (
-     14     torch.stack(batch_features).to(device), torch.stack(batch_eqp_ids).to(device), torch.stack(batch_labels).to(device)
-     15 )
----> 16 outputs, attention_scores = model(batch_features, batch_eqp_ids)
-     17 # print('batch_labels', batch_labels)
-     18 loss = criterion(outputs.squeeze(), batch_labels)
+Cell In[10], line 46
+     43 true_labels = torch.tensor(true_labels).detach().cpu().numpy().reshape(-1, 1)
+     45 # 상관계수 계산
+---> 46 correlation = np.corrcoef(true_labels, predictions)[0, 1]
+     47 print(f"Correlation Coefficient (True Labels vs Predictions): {correlation:.4f}")
+     48 return correlation, predictions, true_labels
 
-File c:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\torch\nn\modules\module.py:1532, in Module._wrapped_call_impl(self, *args, **kwargs)
-   1530     return self._compiled_call_impl(*args, **kwargs)  # type: ignore[misc]
-   1531 else:
--> 1532     return self._call_impl(*args, **kwargs)
+File c:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\numpy\lib\function_base.py:2889, in corrcoef(x, y, rowvar, bias, ddof, dtype)
+   2885 if bias is not np._NoValue or ddof is not np._NoValue:
+   2886     # 2015-03-15, 1.10
+   2887     warnings.warn('bias and ddof have no effect and are deprecated',
+...
+-> 2747 c = dot(X, X_T.conj())
+   2748 c *= np.true_divide(1, fact)
+   2749 return c.squeeze()
 
-File c:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\torch\nn\modules\module.py:1541, in Module._call_impl(self, *args, **kwargs)
-   1536 # If we don't have any hooks, we want to skip the rest of the logic in
-   1537 # this function, and just call forward.
-   1538 if not (self._backward_hooks or self._backward_pre_hooks or self._forward_hooks or self._forward_pre_hooks
-   1539         or _global_backward_pre_hooks or _global_backward_hooks
-   1540         or _global_forward_hooks or _global_forward_pre_hooks):
--> 1541     return forward_call(*args, **kwargs)
-   1543 try:
-   1544     result = None
-
-Cell In[14], line 88
-     87 def forward(self, features, eqp_ids):
----> 88     feature_embed = self.feature_embedding(features)
-     90     eqp_embed = self.eqp_embedding(eqp_ids).unsqueeze(1)  # (batch_size, 1, d_model)
-     92     combined_features = feature_embed + eqp_embed  # Combine feature and equipment embeddings
-
-File c:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\torch\nn\modules\module.py:1532, in Module._wrapped_call_impl(self, *args, **kwargs)
-   1530     return self._compiled_call_impl(*args, **kwargs)  # type: ignore[misc]
-   1531 else:
--> 1532     return self._call_impl(*args, **kwargs)
-
-File c:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\torch\nn\modules\module.py:1541, in Module._call_impl(self, *args, **kwargs)
-   1536 # If we don't have any hooks, we want to skip the rest of the logic in
-   1537 # this function, and just call forward.
-   1538 if not (self._backward_hooks or self._backward_pre_hooks or self._forward_hooks or self._forward_pre_hooks
-   1539         or _global_backward_pre_hooks or _global_backward_hooks
-   1540         or _global_forward_hooks or _global_forward_pre_hooks):
--> 1541     return forward_call(*args, **kwargs)
-   1543 try:
-   1544     result = None
-
-File c:\Users\SKsiltron\AppData\Local\Programs\Python\Python312\Lib\site-packages\torch\nn\modules\linear.py:116, in Linear.forward(self, input)
-    115 def forward(self, input: Tensor) -> Tensor:
---> 116     return F.linear(input, self.weight, self.bias)
-
-RuntimeError: mat1 and mat2 shapes cannot be multiplied (4608x28 and 1x256)
+MemoryError: Unable to allocate 862. GiB for an array with shape (340212, 340212) and data type float64
