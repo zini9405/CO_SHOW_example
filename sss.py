be07c788d3b268695955fe1f45d37fdd98a92cf1
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-03-17 13:54:50 +0900

    sss.py 업데이트

--- sss.py
+++ sss.py
@@ -1,21 +1,29 @@
-def standardize_new_data(new_df, scaler_path):
+import pickle
+import pandas as pd
+
+def normalize_new_data(new_df, scaler_path):
     # 저장된 스케일러 불러오기 (dict 형태)
     with open(scaler_path, 'rb') as f:
         scaler = pickle.load(f)
 
-    # 표준화 제외할 열
+    # 정규화 제외할 열
     exclude_columns = ['ANALYSIS_GROUP', 'SUBLOT', 'WAFER_ID', 'DATE', 'EQP_NM', 'Delta_SFQR', 'CL_HST_REG_DTTM']
 
     # 숫자형 열 중 제외할 열을 제외한 리스트
     numerical_columns = [col for col in new_df.select_dtypes(include=['number']).columns if col not in exclude_columns]
     
-    # 표준화 적용 (X_scaled = (X - mean) / scale)
-    new_df_standardized = new_df.copy()
+    # 정규화 적용 (X_scaled = (X - min) / (max - min))
+    new_df_normalized = new_df.copy()
     
     for col in numerical_columns:
         if col in scaler:
-            mean = scaler[col]['mean']
-            scale = scaler[col]['scale']
-            new_df_standardized[col] = (new_df[col] - mean) / scale  # 직접 표준화 수식 적용
+            min_val = scaler[col]['min']
+            max_val = scaler[col]['max']
+            
+            # min과 max가 동일한 경우 0으로 설정 (안전 처리)
+            if max_val - min_val == 0:
+                new_df_normalized[col] = 0
+            else:
+                new_df_normalized[col] = (new_df[col] - min_val) / (max_val - min_val)
 
-    return new_df_standardized
+    return new_df_normalized
