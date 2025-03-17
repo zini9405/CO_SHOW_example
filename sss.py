b1fbc3ab5deeefa58e22cf806dfb1c0170c03b0e
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-03-17 14:39:27 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -1,1 +1,11 @@
-DtypeWarning: Columns (515) have mixed types. Specify dtype option on import or set low_memory=False.
+x.csv 파일에 ANALYSIS_GROUP열에
+SMNC_L40
+SEC BSI_6_5um_P+
+SEC_L4	
+SMIC_L55	
+HH wuxi	
+GFs_14nm	
+SMNC_L55	
+값을 가지는 행만 필터링함.
+지금 열이 500정도 있어. 너무 많아서 학습하는 데 시간이 오래걸려. 너가 특정열에 큰 변화가 없으면 없애주는 코드 구현해줘.(DELTA_SFQR열은 제외)
+그리고 열 이름에 DEPO라는 문자가 들어간 열만 추출해줘.
