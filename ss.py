f57677deb1cf0bd63ded0003ab1d48989b5f4f85
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-03-24 15:21:43 +0900

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,29 +1,34 @@
+torch.Size([1249244])
+torch.Size([312310])
+Training Epoch 1/2000:   0%|          | 0/1 [00:00<?, ?it/s]
 ---------------------------------------------------------------------------
-TypeError                                 Traceback (most recent call last)
-Cell In[17], line 78
+RuntimeError                              Traceback (most recent call last)
+Cell In[27], line 78
      76 if __name__ == "__main__":
      77     set_seed(42)
 ---> 78     main()
 
-Cell In[17], line 54
-     50     df["RECIPE_ID"] = df["RECIPE_ID"].map(recipe_mapping)
-     52 dataset = CustomDataset(df, eqp_mapping)
----> 54 train_dataset, val_dataset = train_val_split(dataset, val_ratio=0.2)
-     56 train_loader = DataLoader(train_dataset, batch_size=512, shuffle=True)
-     57 val_loader = DataLoader(val_dataset, batch_size=512, shuffle=False)
+Cell In[27], line 65
+     62 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+     63 criterion = nn.CrossEntropyLoss()
+---> 65 train_model_inputs(
+     66     model=model,
+     67     train_loader=train_loader,
+     68     val_loader=val_loader,
+     69     criterion=criterion,
+     70     optimizer=optimizer,
+     71     num_epochs=2000,
+     72     device=device,
+     73     save_path=save_path
+     74 )
 
-Cell In[14], line 10
-      7 val_size = int(len(dataset) * val_ratio)
-      8 train_size = len(dataset) - val_size
----> 10 train_dataset = dataset[:train_size]  # 앞부분을 train으로
-     11 val_dataset = dataset[train_size:]  # 뒷부분을 val로
-     13 return train_dataset, val_dataset
+Cell In[25], line 10
+      8 running_loss = 0.0
+      9 train_pbar = tqdm(train_loader, desc=f"Training Epoch {epoch + 1}/{num_epochs}")
+---> 10 for batch_features, batch_eqp_ids, batch_labels in train_pbar:
+...
+    211     storage = elem._typed_storage()._new_shared(numel, device=elem.device)
+    212     out = elem.new(storage).resize_(len(batch), *list(elem.size()))
+--> 213 return torch.stack(batch, 0, out=out)
 
-Cell In[13], line 27
-     25 def __getitem__(self, idx):
-     26     features = torch.tensor(self.features[idx], dtype=torch.float32)
----> 27     label = torch.tensor(int(self.labels[idx]), dtype=torch.long)
-     28     eqp_id = torch.tensor(self.eqp_ids[idx], dtype=torch.long)
-     29     return features, eqp_id, label
-
-TypeError: only length-1 arrays can be converted to Python scalars
+RuntimeError: stack expects each tensor to be equal size, but got [1249244, 32] at entry 0 and [1249244] at entry 1
