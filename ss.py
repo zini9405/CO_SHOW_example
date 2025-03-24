2eb5f04c317c75d2b0fd5893bef3e8b4ee7777f1
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-03-24 16:15:00 +0900

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,30 +1,73 @@
-RuntimeError                              Traceback (most recent call last)
-Cell In[32], line 78
-     76 if __name__ == "__main__":
-     77     set_seed(42)
----> 78     main()
+        self.fc = nn.Linear(d_model, 2) # 2로 수정
 
-Cell In[32], line 65
-     62 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-     63 criterion = nn.CrossEntropyLoss()
----> 65 train_model_inputs(
-     66     model=model,
-     67     train_loader=train_loader,
-     68     val_loader=val_loader,
-     69     criterion=criterion,
-     70     optimizer=optimizer,
-     71     num_epochs=2000,
-     72     device=device,
-     73     save_path=save_path
-     74 )
 
-Cell In[30], line 22
-     19 print(outputs.shape)
-     20 # print('batch_labels', batch_labels)
-     21 # loss = criterion(outputs.squeeze(), batch_labels)
-...
-   3084 if size_average is not None or reduce is not None:
-   3085     reduction = _Reduction.legacy_get_string(size_average, reduce)
--> 3086 return torch._C._nn.cross_entropy_loss(input, target, weight, _Reduction.get_enum(reduction), ignore_index, label_smoothing)
+class CustomDataset(Dataset):
+    def __init__(self, df, eqp_mapping):
+        self.data = df
+        self.eqp_mapping = eqp_mapping
+        self.labels = self.data['SFQR_AFS2'].values
+        # 정규 분포로 변환
+        # self.quantile_transformer = QuantileTransformer(output_distribution='normal', random_state=42)
+        # transformed_labels = self.quantile_transformer.fit_transform(self.labels.reshape(-1, 1)).flatten()
 
-RuntimeError: 0D or 1D target tensor expected, multi-target not supported
+        # 0.5 기준으로 이진 분류 라벨 생성
+        self.labels = (self.labels > 0.5).astype(np.int64)
+
+        # Feature 추출
+        self.features = self.data.drop(columns=[
+            'WAF_ID', 'HST_REG_DTTM', 'SFQR_AFS2', 'ESFQR2_MAX_AFS2', 'ZDDFRONTMEAN_01_AFS2',
+            'ESFQD_ZONE1MEAN_AFS2', 'ESFQD2_ZONE1MEAN_AFS2', 'SFQR_AFS2_SUB', 'ESFQR2_MAX_AFS2_SUB', 'ESFQD_ZONE1MEAN_AFS2_SUB',
+            'ESFQD2_ZONE1MEAN_AFS2_SUB', 'EQP_ID_MODULE_NAME'
+        ]).values
+        # Equipment ID 인코딩
+        self.eqp_ids = self.data['EQP_ID_MODULE_NAME'].map(self.eqp_mapping).values
+
+    def __len__(self):
+        return len(self.data)
+
+    # def __getitem__(self, idx):
+    #     features = torch.tensor(self.features[idx], dtype=torch.float32)
+    #     label = torch.tensor(self.labels[idx], dtype=torch.long)
+    #     print(label.shape)
+    #     eqp_id = torch.tensor(self.eqp_ids[idx], dtype=torch.long)
+    #     return features, eqp_id, label
+    
+    def __getitem__(self, idx):
+        features = torch.tensor(self.features[idx], dtype=torch.float32)
+        eqp_id = torch.tensor(self.eqp_ids[idx], dtype=torch.long)
+        # label = torch.tensor(self.labels[idx], dtype=torch.long).unsqueeze(0)
+        label = int(self.labels[idx])
+
+        # 디버깅 출력
+        print("features.shape:", features.shape)
+        print("eqp_id:", eqp_id)
+        print("label:", label)
+
+        return features, eqp_id, label
+
+
+# 데이터 매핑 함수
+def map_to_numeric(values):
+    mapping = {val: idx for idx, val in enumerate(sorted(values))}
+    return mapping
+
+def train_val_split(dataset, val_ratio=0.2):
+    val_size = int(len(dataset) * val_ratio)
+    train_size = len(dataset) - val_size
+    
+    indices = list(range(len(dataset)))
+    train_indices = indices[:train_size]
+    val_indices = indices[train_size:]
+
+    train_dataset = Subset(dataset, train_indices)
+    val_dataset = Subset(dataset, val_indices)
+    # train_dataset = dataset[:train_size]  # 앞부분을 train으로
+    # val_dataset = dataset[train_size:]  # 뒷부분을 val로
+    
+    return train_dataset, val_dataset
+
+
+loss = criterion(outputs, batch_labels)
+
+
+criterion = nn.CrossEntropyLoss()
