cd61e24fa031ecf6c348b235ff1487a5be95c700
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-03-24 16:04:57 +0900

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,2 +1,31 @@
-print(batch_features.shape, batch_eqp_ids.shape, batch_labels.shape)torch.Size([512, 32]) torch.Size([512]) torch.Size([512])
-print(outputs.shape) torch.Size([512, 2])
+---------------------------------------------------------------------------
+TypeError                                 Traceback (most recent call last)
+Cell In[14], line 78
+     76 if __name__ == "__main__":
+     77     set_seed(42)
+---> 78     main()
+
+Cell In[14], line 65
+     62 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+     63 criterion = nn.CrossEntropyLoss()
+---> 65 train_model_inputs(
+     66     model=model,
+     67     train_loader=train_loader,
+     68     val_loader=val_loader,
+     69     criterion=criterion,
+     70     optimizer=optimizer,
+     71     num_epochs=2000,
+     72     device=device,
+     73     save_path=save_path
+     74 )
+
+Cell In[12], line 10
+      8 running_loss = 0.0
+      9 train_pbar = tqdm(train_loader, desc=f"Training Epoch {epoch + 1}/{num_epochs}")
+---> 10 for batch_features, batch_eqp_ids, batch_labels in train_pbar:
+...
+   (...)
+   1055             stacklevel=2,
+   1056         )
+
+TypeError: iteration over a 0-d tensor
