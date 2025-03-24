379ca7c240334639c327d0c97080bc4dfa8c91b7
Junwon Lee <65759847+zini9405@users.noreply.github.com>
2025-03-24 14:51:34 +0900

    Update sss.py

--- sss.py
+++ sss.py
@@ -138,3 +138,26 @@ def compute_variable_importance(attention_scores):
         device=device,
         save_path=save_path
     )
+
+
+# Custom Dataset 정의
+class CustomDataset(Dataset):
+    def __init__(self, df, eqp_mapping):
+        self.data = df
+        self.labels = self.data['6900_GBIR_AFS2'].values
+        self.eqp_mapping = eqp_mapping
+        self.quantile_transformer = QuantileTransformer(output_distribution='normal', random_state=42)
+        self.labels = self.quantile_transformer.fit_transform(self.labels.reshape(-1, 1)).flatten()
+        self.features = self.data.drop(columns=['BASE_DT', 'EQP_ID', 'WAF_ID', '6900_GBIR_AFS2']).values
+
+
+        self.eqp_ids = self.data['EQP_ID'].map(self.eqp_mapping).values
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        features = torch.tensor(self.features[idx], dtype=torch.float32)
+        label = torch.tensor(self.labels[idx], dtype=torch.float32)
+        eqp_id = torch.tensor(self.eqp_ids[idx], dtype=torch.long)
+        return features, eqp_id, label
